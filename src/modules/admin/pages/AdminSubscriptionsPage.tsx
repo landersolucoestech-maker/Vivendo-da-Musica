aa2f0e9cca289524e3e4247a80036df16a01d282
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-01 21:48:54 -0300

    refactor: remove unsupported subscriptions page

--- src/modules/admin/pages/AdminSubscriptionsPage.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { ROUTES } from '@/shared/constants/routes';
-
-const AdminSubscriptionsPage = () => <Navigate to={ROUTES.admin} replace />;
-
-export default AdminSubscriptionsPage;
