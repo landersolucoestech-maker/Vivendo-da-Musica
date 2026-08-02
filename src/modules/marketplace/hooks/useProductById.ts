b8e87018ca33f1f05685debf82a4f039c5674515
landersolucoestech-maker <landersolucoestech@gmail.com>
2026-08-01 21:50:34 -0300

    refactor: remove mock product query key

--- src/modules/marketplace/hooks/useProductById.ts
+++ src/modules/marketplace/hooks/useProductById.ts
@@ -1,9 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import { marketplaceService } from "@/modules/marketplace/services/marketplace.service";
+import { useQuery } from '@tanstack/react-query';
+import { marketplaceService } from '@/modules/marketplace/services/marketplace.service';
 
 export const useProductById = (id: string | undefined) => {
   return useQuery({
-    queryKey: ['product-by-id', 'mock', id],
+    queryKey: ['product-by-id', id],
     queryFn: () => marketplaceService.getProductById(id!),
     enabled: !!id,
   });
